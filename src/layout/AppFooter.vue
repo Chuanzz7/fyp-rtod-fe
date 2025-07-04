36c5fd3e1cdb22020f61a53d4d8c5ecfd6f8866e
ChuanATF <chngkahchuan@altecflex.com>
2025-07-04 17:53:23 +0800

    footer

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -2,7 +2,7 @@
 
 <template>
     <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+        Smart Inventory by
+        <p class="text-primary font-bold">Chuan</p>
     </div>
 </template>
